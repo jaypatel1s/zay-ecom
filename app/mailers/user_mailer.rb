e17b1eb026f5717674afdbbf7ea5e79460794503
tejasraut <tejasraut@softices.com>
2022-12-29 11:56:17 +0530

    last

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,13 +1,19 @@
 class UserMailer < ApplicationMailer
   default from: 'rauttejas066@gmail.com'
 
-  def welcome_email(user)
+  def login_email(user)
     @user = user
     mail(to: @user.email, subject: "logged in as #{user.email}")
   end
-
+           
   def logout_email(user)
     @user = user
     mail(to: @user.email, subject: "logged out as #{@user.email}")
+  end
+
+  def welcome_email
+    @user = params[:user]
+    @url =  'http://127.0.0.1:3000/users/sign_in'
+    mail(to: @user.email, subject:"Welcome to my awesome site")
   end
 end
